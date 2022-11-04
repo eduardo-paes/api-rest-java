e88f2312af9f75ee8e1b6264dfdd18f0070e669f
eduardo <eduardo.paes@lektek.com.br>
2022-11-04 20:09:38 -0300

    feat: add password encryptation support

--- src/main/java/copetsystem/api/controller/UserController.java
+++ src/main/java/copetsystem/api/controller/UserController.java
@@ -2,10 +2,12 @@ package copetsystem.api.controller;
 
 import copetsystem.api.model.User;
 import copetsystem.api.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -24,5 +26,19 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/validate")
+    public  ResponseEntity<Boolean> validatePassword(@RequestParam String login, @RequestParam String password) {
+        Optional<User> optionalUser = service.findByLogin(login);
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        User user = optionalUser.get();
+        boolean valid = service.validatePassword(password, user.getPassword());
+
+        HttpStatus status = valid ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
     }
 }
--- src/main/java/copetsystem/api/service/UserService.java
+++ src/main/java/copetsystem/api/service/UserService.java
@@ -3,23 +3,36 @@ package copetsystem.api.service;
 import copetsystem.api.model.Imc;
 import copetsystem.api.model.User;
 import copetsystem.api.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
-    private UserRepository repository;
+    private final UserRepository repository;
+    private final PasswordEncoder encoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
+        this.encoder = encoder;
     }
 
     public User add(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return this.repository.save(user);
     }
 
     public List<User> findAll() {
         return this.repository.findAll();
+    }
+
+    public Optional<User> findByLogin(String login) {
+        return this.repository.findByLogin(login);
+    }
+
+    public boolean validatePassword(String password, String userPassword) {
+        return encoder.matches(password, userPassword);
     }
 }
